614d9fa4eb61b8c957565774d42abd2436357292
Matt Glaman <nmd.matt@gmail.com>
2013-07-03 00:37:12 -0500

    Add comments template, delete content-* templates
    Default post format templates were going to be included, but that
    should be left to theme developers to include.
    
    Added basic comment template

--- comments.php
+++ comments.php
@@ -5,4 +5,34 @@
  * @package Flagship
  * @since Flagship 0.1
  */
+if ( post_password_required() )
+	return;
 ?>
+<div id="comments" class="comments-area">
+
+	<?php // You can start editing here -- including this comment! ?>
+
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title"><?php get_comments_number(); ?> </h2>
+
+		<ol class="commentlist">
+			<?php wp_list_comments( array( 'style' => 'ol' ) ); ?>
+		</ol><!-- .commentlist -->
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below" class="navigation" role="navigation">
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
+
+		<?php
+		if ( ! comments_open() && get_comments_number() ) : ?>
+		<p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
+		<?php endif; ?>
+
+	<?php endif; // have_comments() ?>
+
+	<?php comment_form(); ?>
+
+</div><!-- #comments .comments-area -->
